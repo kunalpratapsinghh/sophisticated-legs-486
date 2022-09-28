84aad6b9cb00541fa78d3b76f8b041370e9829c5
Debasree Ghosh <97526617+Debasree-3031999@users.noreply.github.com>
2022-09-28 22:36:30 +0530

    day 2 done

---
+++ bigbasket/src/component/Homepage/Slider_config.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SliderElem = styled.div`
+  width: 100%;
+  img {
+    width: 100%;
+  }
+`;
+
+export const SliderElemBottom = styled.div`
+  width: 85%;
+  margin: 20px auto 0;
+  @media screen and (max-width: 870px) {
+    width: 80%;
+  }
+`;
+
+
