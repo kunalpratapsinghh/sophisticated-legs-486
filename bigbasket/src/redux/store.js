abe7c28e1aa742bd4c31aa8c9c4f296736bf0f3e
shashankkumarP <nawalerason@gmail.com>
2022-09-30 06:11:36 +0530

    try2

--- bigbasket/src/redux/store.js
+++ bigbasket/src/redux/store.js
@@ -7,6 +7,7 @@ import {Cart_reducer} from "../pages/cart/Cart_reducer"
 
 const rootreducer=combineReducers({
     cart:Cart_reducer,
-    reducer1:reducer
+    reducer1:reducer,
+    
 })
 export const store =legacy_createStore(rootreducer,applyMiddleware(thunk))
