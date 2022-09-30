00a5f4fc7e314191b7e070079a79e33325e9621e
AshwiniPerewar@gmail.com <ashwiniperewar411@gmail.com>
2022-09-30 23:54:09 +0530

    add to cart working

--- server/Router/Router.js
+++ server/Router/Router.js
@@ -28,6 +28,38 @@ router.get("/cart",async (req,res)=>{
 
 
 })
+
+
+
+router.post("/cart",async(req,res)=>{
+    
+    try{
+        let cart = new cart_model(req.body);
+        await cart.save();
+        return res.status(201).send("Product added to cart successfully");
+
+    }catch(e){
+        res.status(400).send("something went wrong")
+
+    }
+})
+//patch
+
+router.patch("/cart/:id",async(req,res)=>{
+    let {id}= req.params.id;
+
+
+    try{
+
+        let data = cart_model.findByIdAndUpdate(id,req.body)
+        data.save();
+        res.send("Succesfully updated");
+        
+
+    }catch(e){
+            res.send(e);
+    }
+})
 ///////////////////////////// product page//////////////////////////////
 
 //get
@@ -41,34 +73,18 @@ router.get("/product",async(req,res)=>{
         res.status(400).send(e.message)
     }
 })
+
+router.get("/product/:id",async(req,res)=>{
+    try{
+        let product = await product_model.findById(req.params.id);
+        res.status(200).send(product);
+
+    }catch(e){
+        res.status(400).send(e.message)
+    }
+})
 ////post
 
-router.post("/cart/:user",async(req,res)=>{
-    
-    // try{
-    //     let cart = new cart_model(...req.body);
-    //     await cart.save();
-    //     return res.status(201).send("successfull");
 
-    // }catch(e){
-    //     res.status(400).send("something went wrong")
-
-    // }
-})
-//patch
-
-// router.patch("/cart/:id",async(req,res)=>{
-//     let {id}= req.params.id;
-
-
-//     try{
-
-//         let data = cart_model.updateOne({_id:id},{});{name:"saurav",cart:[]}
-        
-
-//     }catch(e){
-
-//     }
-// })
 
 module.exports = router;
