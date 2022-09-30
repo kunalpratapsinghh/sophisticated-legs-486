df170fcf61361d4a8078a7f44a3b5798fa159b55
Debasree Ghosh <97526617+Debasree-3031999@users.noreply.github.com>
2022-09-30 17:10:46 +0530

    CLEAR

--- bigbasket/src/App.js
+++ bigbasket/src/App.js
@@ -3,12 +3,14 @@ import { Footer } from "./component/Footer";
 import { Topnavbar } from "./component/Topnavbar";
 // import {useSelector } from "react-redux";
 // import{Box,Heading} from "@chakra-ui/react"
+
 import Router from "./pages/Allroutes";
 function App() {
   // const {name}= useSelector((state) => state.reducer1);
   return (
     <div className="App">
       <Topnavbar />
+     
       <Router />
       <Footer />
     </div>
