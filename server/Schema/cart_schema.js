30cd3769d1bebd8be8c5b9b81870efcd714b1061
shashankkumarP <nawalerason@gmail.com>
2022-10-03 14:00:19 +0530

    final_3

--- server/Schema/cart_schema.js
+++ server/Schema/cart_schema.js
@@ -5,7 +5,7 @@ const Cart_schema= new Schema({
     product_id:Schema.Types.ObjectId,
     Title:String,
     Brand:String,
-    count:{type:Number,default:1},
+    count:{type:Number,default:1}, 
     Image_url:String,
     Price:Number,
     Category:String
