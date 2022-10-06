cb958dd0c28ce74f71ded5d4e1e933284125a27a
Shashank kumar <67480861+shashankkumarP@users.noreply.github.com>
2022-10-07 02:08:22 +0530

    Revert "my_own"

--- bigbasket/src/pages/cart/Cart_action.js
+++ bigbasket/src/pages/cart/Cart_action.js
@@ -15,52 +15,6 @@ export const getcart=(userid)=>async(dispatch)=>{
 
     
 }
-//////////////////////decrease cart data////////////////
-
-export const deccart=(el,op)=>async(dispatch)=>{
-    
-    try{
-        await axios.post(`http://localhost:8080/cartchange/${el.user_id}`,{
-        product_id:el.product_id,
-        op:op
-    });
-    dispatch(getcart(el.user_id));
-
-    }catch(e){
-        dispatch({type:cart_error})
-      }
-
-
-}
-/////////delete from cart ///////////////
-
-export const handledelete = (el)=>async(dispatch)=>{
-    console.log(el);
-     
-      try{
-        await axios.post(`http://localhost:8080/cart/${el.user_id}`,{
-        product_id:el.product_id
-      });
-      dispatch(getcart(el.user_id));
-
-      }catch(e){
-        dispatch({type:cart_error})
-      }
-}
-
-export const handleuserdelete = (userid)=>async(dispatch)=>{
-
-    console.log("dekho user delete click kr diya")
-   try{
-    await axios.delete(`http://localhost:8080/cartuserdelete/${userid}`);
-    dispatch({type:cart_add_successfull,payload:[]})
-
-   }catch(e){
-    dispatch({type:cart_error})
-  }
-    
-
-}
 
 
 
